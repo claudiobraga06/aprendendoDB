a05ae7a66c8630ae73f671fe600ebda7c1bb9abd
claudiobraga06@gmail.com <claudiobraga06@gmail.com>
2023-01-19 13:17:42 +0000

    Atualização com o DB5

--- apresentacao1.py
+++ apresentacao1.py
@@ -12,17 +12,24 @@ from databricks import sql
 
 # COMMAND ----------
 
+# server_hostname deve ser obtido em connection details do warehouse
+# http_path deve ser obtido em connection datails do warehouse
+# access_token deve ser obtido em user settings do usuário.
 
 
-with sql.connect(server_hostname = "adb-584769158701554.14.azuredatabricks.net",
-                 http_path       = "/sql/1.0/warehouses/8258c14297e7f530",
-                 access_token    = "dapi7049e84ab4ae1b9fb73cbb4c09f3bc88-3") as connection:
+
+
+with sql.connect(server_hostname = "adb-4571612221051173.13.azuredatabricks.net",
+                 http_path       = "/sql/1.0/warehouses/339b21f7a3803635",
+                 access_token    = "dapid7b50090a580f7f6b7cbd440d4bf8f7c-3") as connection:
+
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM default.produtos LIMIT 10")
+        cursor.execute("select * from rfb.sandbox.numeros")
         result = cursor.fetchall()
 
         for row in result:
           print(row)
+ 
  
 
 # COMMAND ----------
@@ -33,18 +40,19 @@ with sql.connect(server_hostname = "adb-584769158701554.14.azuredatabricks.net",
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC use default;
+# MAGIC 
+# MAGIC show catalogs
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use rfb.sandbox;
 # MAGIC show tables;
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC select * from produtos
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select Categorias,COUNT(*) from produtos group by Categorias
+# MAGIC select * from numeros
 
 # COMMAND ----------
 
@@ -54,13 +62,17 @@ with sql.connect(server_hostname = "adb-584769158701554.14.azuredatabricks.net",
 # COMMAND ----------
 
 # MAGIC %python
-# MAGIC filePath = "dbfs:/user/hive/warehouse/produtos"
+# MAGIC # filePath deve ser igual a uma external location.
+# MAGIC 
+# MAGIC filePath = "abfss://tables@externaltablesdb5.dfs.core.windows.net/"
 # MAGIC display(dbutils.fs.ls(filePath))
 
 # COMMAND ----------
 
 # MAGIC %python
-# MAGIC df = spark.read.format("delta").load(filePath)
+# MAGIC filePath = "abfss://tables@externaltablesdb5.dfs.core.windows.net/numeros.csv"
+# MAGIC df = spark.read.format("csv").load(filePath)
+# MAGIC display (df)
 
 # COMMAND ----------
 
@@ -68,23 +80,21 @@ from pyspark.sql.functions import col, translate,lit,when,avg
 
 # COMMAND ----------
 
-df2 = df.withColumn("compar",
+display(df)
+
+# COMMAND ----------
+
+df2 = df.withColumn("_c0",
                     when(
-                        col("segmento")=="Inverno",lit("sim")).
+                        col("_c0")==3,lit("sim")).
                         otherwise(lit("não"))
                    )
+display (df2)
 
 # COMMAND ----------
 
-display(df2)
-
-# COMMAND ----------
-
-df2 =  df.groupby("categorias").count()
-
-# COMMAND ----------
-
-display(df2 )
+df3 =  df2.groupby("_c0").count()
+display(df3)
 
 # COMMAND ----------
 
